cec126344c1c26b4434477736e451956645ea312
wickramDSK <wickdsk.2k@gmail.com>
2024-11-10 16:54:23 +0530

    Create Theater repository interface

---
+++ src/main/java/com/bkticketing/bkTicketing_backend/Repository/TheaterRepository.java
@@ -0,0 +1,9 @@
+package com.bkticketing.bkTicketing_backend.Repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.bkticketing.bkTicketing_backend.Model.Theater;
+
+public interface TheaterRepository extends MongoRepository<Theater, String> {
+
+}
