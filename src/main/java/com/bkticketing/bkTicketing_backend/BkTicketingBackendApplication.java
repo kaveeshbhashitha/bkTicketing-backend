45aebdf2d99827690e12e0849dc5d57982dbdbc3
kaveeshbhashitha <kaveeshbhashitha@gmail.com>
2024-11-12 14:18:29 +0530

    solve conflicts

--- src/main/java/com/bkticketing/bkTicketing_backend/BkTicketingBackendApplication.java
+++ src/main/java/com/bkticketing/bkTicketing_backend/BkTicketingBackendApplication.java
@@ -1,5 +1,4 @@
 package com.bkticketing.bkTicketing_backend;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +8,4 @@ public class BkTicketingBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BkTicketingBackendApplication.class, args);
 	}
-
 }
