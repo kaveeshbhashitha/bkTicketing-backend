77f6ac416a5b0b4651089b0e15ed16e47bda961c
kaveeshbhashitha <kaveeshbhashitha@gmail.com>
2024-11-13 16:58:56 +0530

    Email and notification part added

---
+++ src/main/java/com/bkticketing/bkTicketing_backend/Service/EmailService.java
@@ -0,0 +1,13 @@
+package com.bkticketing.bkTicketing_backend.Service;
+
+import com.bkticketing.bkTicketing_backend.Model.Email;
+
+import java.util.List;
+import java.util.Optional;
+public interface EmailService {
+    Email addEmail(Email email);
+    List<Email> getAllEmail();
+    Optional<Email> getEmailById(String emailId);
+    Email updateEmail(String emailId, Email emailDetails);
+    void deleteEmail(String emailId);
+}
