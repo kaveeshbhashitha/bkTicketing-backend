f87e6b41f18b4ef24e513f134784d1e35d639610
Bhashitha Kaveesh <159679734+kaveeshbhashitha@users.noreply.github.com>
2024-11-15 17:05:51 +0530

    Update crossorigin changes GeneralEventController.java

--- src/main/java/com/bkticketing/bkTicketing_backend/Controller/GeneralEventController.java
+++ src/main/java/com/bkticketing/bkTicketing_backend/Controller/GeneralEventController.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000"})
+@CrossOrigin("http://localhost:3000")
 @RequestMapping("/generalEvent")
 public class GeneralEventController {
     @Autowired
